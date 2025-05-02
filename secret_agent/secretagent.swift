ada0e236c726a1d1427a24aa9f123c1db83ccf77
Ewoma <rewoma19@gmail.com>
2025-05-02 06:58:38 +0000

    Complete code for secret agent challenge

--- secret_agent/secretagent.swift
+++ secret_agent/secretagent.swift
@@ -1,7 +1,24 @@
+// HIGHER ORDER FUNCTIONS
+
 func protectSecret(_ secret: String, withPassword password: String) -> (String) -> String {
-  
+  func checkPassword(_ enteredPassword: String) -> String {
+    
+    if password == enteredPassword {
+      return secret
+    } else {
+      return "Sorry. No hidden secrets here."
+    }
+    
+  }
+
+  return checkPassword
 }
 
 func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
   
+  let firstCode = f(room)
+  let secondCode = f(firstCode)
+  let thirdCode = f(secondCode)
+
+  return (firstCode, secondCode, thirdCode)
 }
