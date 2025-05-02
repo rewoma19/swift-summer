f2769ed77a20f4257e3a63d8a7cf8ab1989233f7
Ewoma <rewoma19@gmail.com>
2025-05-02 05:44:11 +0000

    Write instructions for secret agent challenge

---
+++ secret_agent/secretagent.swift
@@ -0,0 +1,7 @@
+func protectSecret(_ secret: String, withPassword password: String) -> (String) -> String {
+  
+}
+
+func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
+  
+}
